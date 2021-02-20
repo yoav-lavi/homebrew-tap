9a0bce14047568d03881a1b94562e532e41a4756
Yoav Lavi <yoavlavi122@gmail.com>
2021-02-20 21:09:29 +0100

    crispr 0.2.0

--- Formula/crispr.rb
+++ Formula/crispr.rb
@@ -1,12 +1,12 @@
 class Crispr < Formula
-  version '0.1.0'
+  version '0.2.0'
   desc "Scaffold a project from a template"
   homepage "https://github.com/yoav-lavi/crispr"
   license "MIT"
 
   if OS.mac?
       url "https://github.com/yoav-lavi/crispr/releases/download/v#{version}/crispr-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "a8e84e536c179d8069f5487d85467b1a0d93f141a3bcf0e31d5e2cec15696517"
+      sha256 "3840d91a8a215455af759c24dd9a2878e713e9ecb64d3ab2ef1f14b497aff83c"
   end
 
   conflicts_with "crispr"
